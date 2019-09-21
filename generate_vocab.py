43dab303fbaad3ba466add5ec05bafb3c2b094bf
rsawada <qhapaq49@yahoo.co.jp>
2019-09-21 16:42:28 +0900

    add control symbols

--- generate_vocab.py
+++ generate_vocab.py
@@ -1,3 +1,3 @@
 import sentencepiece as spm
 import sys
-spm.SentencePieceTrainer.Train("--input=" + sys.argv[1] +" --model_prefix=m --add_dummy_prefix=false --vocab_size=2000 --character_coverage=0.9999999")
+spm.SentencePieceTrainer.Train("--input=" + sys.argv[1] +" --model_prefix=m --add_dummy_prefix=false --vocab_size=2000 --character_coverage=0.9999999 --control_symbols=[PAD],[CLS],[SEP],[MASK]")
